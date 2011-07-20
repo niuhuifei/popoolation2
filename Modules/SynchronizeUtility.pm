0cbef52fb3f2113ed2afe83cf05301fc0aae34e5
RoKofler@gmail.com <RoKofler@gmail.com@0c14fcf6-89ed-1be7-9e52-194e74530410>
2011-07-20 09:49:42 +0000

    popoolation2 available, initial commit, woohoo

---
+++ Modules/SynchronizeUtility.pm
@@ -0,0 +1,58 @@
+{
+    package SynchronizeUtility;
+    use strict;
+    use warnings;
+    use FindBin qw/$Bin/;
+    use lib "$Bin";
+    use Synchronized;
+    use Test;
+    
+    require Exporter;
+    our @ISA = qw(Exporter);
+    our @EXPORT =qw(format_parsed_pileup get_popcount_forsyncfile format_synchronized);
+    
+    
+    sub format_synchronized
+    {
+        my $sync=shift;
+        
+        my @temp=();
+        # chr, pos, refchar, minsampcov, samples (A T C G N del)
+        push @temp,$sync->{chr};
+        push @temp,$sync->{pos};
+        push @temp,$sync->{refchar};
+        foreach my $s (@{$sync->{samples}})
+        {
+            
+            push @temp,format_parsed_pileup($s);
+        }
+        
+        my $toret=join("\t",@temp);
+        return $toret;
+    }
+    
+    sub format_parsed_pileup
+    {
+        my $pp=shift;
+        return "$pp->{A}:$pp->{T}:$pp->{C}:$pp->{G}:$pp->{N}:$pp->{del}" if $pp;
+        return "-";
+    }
+    
+    sub get_popcount_forsyncfile
+    {
+        my $syncfile=shift;
+        open my $ifh, "<", $syncfile or die "Could not open syncfile";
+        my $pp=get_basic_syncparser();
+        my $pl=$pp->(<$ifh>);
+        my $count=scalar(@{$pl->{samples}});
+        close $ifh;
+        return $count;
+        
+        
+    }
+    
+
+
+}
+
+1;
